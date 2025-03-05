b3e5da955bc0eef973151155f16af1acbf28d39a
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-05 23:50:57 +0300

    Reverse a String added

---
+++ Legacy-JavaScript-Algorithms-and-Data-Structures/3-Basic-Algorithm-Scripting/02-Reverse-a-String.js
@@ -0,0 +1,13 @@
+// ❓DESCRIPTION
+// Reverse the provided string and return the reversed string.
+// For example, "hello" should become "olleh".
+
+
+// ✅ SOLUTION
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+
+// ✅ Checking
+console.log(reverseString('hello'));
