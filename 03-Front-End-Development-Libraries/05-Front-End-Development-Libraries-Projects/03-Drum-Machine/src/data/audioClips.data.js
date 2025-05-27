cc088917195661efc830f87ccec0f4d282219892
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-28 00:43:21 +0300

    Drum Machine - drumPads array added

---
+++ 03-Front-End-Development-Libraries/05-Front-End-Development-Libraries-Projects/03-Drum-Machine/src/data/audioClips.data.js
@@ -0,0 +1,21 @@
+import Heater1 from '../assets/audio/Heater-1.mp3'
+import Heater2 from '../assets/audio/Heater-2.mp3'
+import Heater3 from '../assets/audio/Heater-3.mp3'
+import Heater4 from '../assets/audio/Heater-4.mp3'
+import Heater6 from '../assets/audio/Heater-6.mp3'
+import OpenHH from '../assets/audio/Open-HH.mp3'
+import KickHat from '../assets/audio/Kick-n-Hat.mp3'
+import Kick from '../assets/audio/Kick.mp3'
+import ClosedHH from '../assets/audio/Closed-HH.mp3'
+
+export const drumPads = [
+  { id: 0, key: 'Q', name: 'Heater 1', audio: Heater1 },
+  { id: 1, key: 'W', name: 'Heater 2', audio: Heater2 },
+  { id: 2, key: 'E', name: 'Heater 3', audio: Heater3 },
+  { id: 3, key: 'A', name: 'Heater 4', audio: Heater4 },
+  { id: 4, key: 'S', name: 'Heater 6', audio: Heater6 },
+  { id: 5, key: 'D', name: 'Open-HH', audio: OpenHH },
+  { id: 6, key: 'Z', name: "Kick-n'-Hat", audio: KickHat },
+  { id: 7, key: 'X', name: 'Kick', audio: Kick },
+  { id: 8, key: 'C', name: 'Closed-HH', audio: ClosedHH },
+]
