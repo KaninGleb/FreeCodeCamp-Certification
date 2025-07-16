4e7d7b2486ec9e2b2f1bc4478156ee7df567a855
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-17 01:25:20 +0300

    Role Playing Game - Steps 96-100

--- 02-JavaScript-Algorithms-and-Data-Structures/Part-1/03-Role-Playing-Game/scripts.js
+++ 02-JavaScript-Algorithms-and-Data-Structures/Part-1/03-Role-Playing-Game/scripts.js
@@ -86,7 +86,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (currentWeaponIndex < 3) {
+  if (currentWeaponIndex < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
@@ -98,6 +98,8 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
 
