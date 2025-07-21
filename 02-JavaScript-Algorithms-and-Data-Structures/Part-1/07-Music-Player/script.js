3e94b89ab6439a5f7b44a069d139b6bd77c265db
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-21 22:17:45 +0300

    Music Player - Steps 16-20

--- 02-JavaScript-Algorithms-and-Data-Structures/Part-1/07-Music-Player/script.js
+++ 02-JavaScript-Algorithms-and-Data-Structures/Part-1/07-Music-Player/script.js
@@ -79,23 +79,21 @@ const allSongs = [
 ];
 
 const audio = new Audio();
+
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
-const printGreeting = () => {
-  console.log('Hello there!');
-}
 
-printGreeting();
+const renderSongs = (array) => {
+  const songsHTML = array
+    .map((song)=> {
+      return `
+      <li id="song-${song.id}" class="playlist-song">
 
-const printMessage = org => {
-  console.log(`${org} is awesome!`);
-}
-
-printMessage('freeCodeCamp');
-
-const addTwoNumbers = (num1, num2) => num1 + num2
-
-console.log(addTwoNumbers(3,4));
+      
+      </li>
+      `;
+    })
+};
