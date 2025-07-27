de8625821ccee10c6c17ee8c66992bb1fccdb03c
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-27 23:00:00 +0300

    Football Team Cards - Steps 6-10

--- 02-JavaScript-Algorithms-and-Data-Structures/Part-2/02-Football-Team-Cards/scripts.js
+++ 02-JavaScript-Algorithms-and-Data-Structures/Part-2/02-Football-Team-Cards/scripts.js
@@ -5,5 +5,22 @@ const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
 const myFavoriteFootballTeam = {
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
+  players: [
+    {
+      name: "Sergio Almirón",
+      position: "forward",
+      number: 1,
+      isCaptain: false,
+      nickname: null,
+    },
 
+  ]
 };
