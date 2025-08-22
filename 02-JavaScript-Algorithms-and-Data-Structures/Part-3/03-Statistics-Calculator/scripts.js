b61c0c3f26448bf81ec82f1dd6eede55af2560f2
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-22 12:00:00 +0300

    Statistics Calculator - Steps 46-50

--- 02-JavaScript-Algorithms-and-Data-Structures/Part-3/03-Statistics-Calculator/scripts.js
+++ 02-JavaScript-Algorithms-and-Data-Structures/Part-3/03-Statistics-Calculator/scripts.js
@@ -30,6 +30,19 @@ const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
 }
 
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const differences = array.map(
+    el => el - mean
+  );
+  const squaredDifferences = differences.map(
+    el => el ** 2
+  );
+  const sumSquaredDifferences = squaredDifferences.reduce(
+    (acc, el) => acc + el, 0
+  );
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -38,10 +51,10 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
-
+  const range = getRange(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
-
+  document.querySelector("#range").textContent = range;
 }
